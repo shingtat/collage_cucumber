970610624fe19812aaf4b03bf2fff085b640df6b
Shing Tak Ryan To <shingtat@usc.edu>
2018-03-07 11:39:26 -0800

    export collage button done

--- features/collage.feature
+++ features/collage.feature
@@ -12,7 +12,7 @@ Scenario: background color
 Scenario: title at top of the page
   When h1 text loads
   Then I should see "black" text
-  Then I should see Reads Collage for topic X where "dog" is the topic
+  Then I should see Reads Collage for topic X where "cat" is the topic
   Then I should see font size is at least "24px"
 
 Scenario: build another collage button
@@ -28,3 +28,11 @@ Scenario: input box
   Then I should see Prompt text disappears when user begins typing in a topic
   Then I should see Background color "white"
   Then I should see Pressing enter in the box triggers the build collage process
+
+Scenario: export collage button
+  When export collage button loads
+  Then I should see Clicking button allows the user to download a PNG image of the collage
+  Then I should see Size of image should match the displayed collage’s size
+  Then I should see Export Collage Button color is "dark gray"
+  Then I should see Export Collage Text color is "white"
+  Then I should see Export Button labeled with text that says "Export Collage"
--- features/step_definitions/collage.rb
+++ features/step_definitions/collage.rb
@@ -25,17 +25,17 @@ Then(/^I should see "([^"]*)" text$/) do |color|
   expect(title.native.style('color')).to eq('rgba(0, 0, 0, 1)')
 end
 
-# Then(/^I should see Reads Collage for topic X where "([^"]*)" is the topic$/) do |arg1|
-#   visit "http://localhost:8080/Implementation/index.html"
-#   text_input = find_by_id("text_input")
-#   fill_in('text_input', :with => arg1)
-#   click_button("build")
-#   find_by_id('history')
-#   url = URI.parse(current_url)
-#   params = CGI.parse(url.query)
-#   topic = params['topic'][0]
-#   expect(topic).to eq(arg1)
-# end
+Then(/^I should see Reads Collage for topic X where "([^"]*)" is the topic$/) do |arg1|
+  visit "http://localhost:8080/Implementation/index.html"
+  text_input = find_by_id("text_input")
+  fill_in('text_input', :with => arg1)
+  click_button("build")
+  find_by_id('history')
+  url = URI.parse(current_url)
+  params = CGI.parse(url.query)
+  topic = params['topic'][0]
+  expect(topic).to eq(arg1)
+end
 
 #Font size is at least 24px
 Then(/^I should see font size is at least "([^"]*)"$/) do |size|
@@ -70,4 +70,37 @@ end
 #Testing for Input Box in Collage page
 When(/^input box loads$/) do
   find_by_id("text_input")
+end
+
+#Testing for Export Collage button in Collage page
+When(/^export collage button loads$/) do
+  find_by_id("export")
+end
+
+#Clicking button allows the user to download a PNG image of the collage
+Then(/^I should see Clicking button allows the user to download a PNG image of the collage$/) do
+  click_button("export")
+  sleep(5)
+  puts File.exist?('/Users/Master/Downloads/downloadedCollage1.png')
+end
+
+#Size of image should match the displayed collage’s size
+Then(/^I should see Size of image should match the displayed collage’s size$/) do
+
+end
+
+Then(/^I should see Export Collage Button color is "([^"]*)"$/) do |color|
+  export = page.find_by_id("export")
+  expect(export.native.style('background-color')).to eq('rgba(102, 102, 102, 1)')
+end
+
+Then(/^I should see Export Collage Text color is "([^"]*)"$/) do |color|
+  export = page.find_by_id("export")
+  puts export.native.style('color')
+  expect(export.native.style('color')).to eq('rgba(255, 255, 255, 1)')
+end
+
+Then(/^I should see Export Button labeled with text that says "([^"]*)"$/) do |text|
+  export = page.find_by_id("export")
+  expect(export.text).to eq(text)
 end
