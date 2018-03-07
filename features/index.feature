62b93d52fcfbdb55919d2f99e12286ca64cbf864
Shing Tak Ryan To <shingtat@usc.edu>
2018-03-07 00:36:34 -0800

    index page css done

--- features/index.feature
+++ features/index.feature
@@ -1,4 +1,3 @@
-@index.rb
 Feature: Index Page Requirements
 
 Background:
@@ -13,3 +12,14 @@ Scenario: background color
 Scenario: input text
   When input text loads
   Then I should see dark gray outline
+  Then I should see centered in the middle of the page
+  Then I should see the search box with placeholder text "Enter Topic"
+  Then I should see Prompt text disappears when user begins typing in a topic
+  Then I should see Background color "white"
+  Then I should see Pressing enter in the box triggers the build collage process
+
+Scenario: build collage button
+  When build collage button loads
+  Then I should see Button labeled with text that says "Build Collage"
+  Then I should see Text color is "white"
+  Then I should see Button color is "dark gray"
--- features/step_definitions/index.rb
+++ features/step_definitions/index.rb
@@ -2,6 +2,7 @@ Given(/^I am on the Index Page$/) do
   visit "http://localhost:8080/Implementation/index.html"
 end
 
+#Testing for White Background
 When(/^html loads$/) do
   find("html")
 end
@@ -12,6 +13,7 @@ Then(/^I should see gray background color$/) do
   expect(html.native.style('background-color')).to eq('rgba(211, 211, 211, 1)')
 end
 
+#Testing for Input Text Box
 When(/^input text loads$/) do
   find_by_id("text_input")
 end
@@ -20,4 +22,44 @@ Then(/^I should see dark gray outline$/) do
   text_input = page.find_by_id("text_input")
   puts text_input.native.style("border-style")
   expect(text_input.native.style('border-style')).to eq('solid')
+end
+
+Then(/^I should see centered in the middle of the page$/) do
+  text_div = page.find_by_id("input")
+  text_div[:class].include?("centered")
+end
+
+Then(/^I should see the search box with placeholder text "([^"]*)"$/) do |placeholdertext|
+  expect(find_field('text_input')['placeholder']).to eq placeholdertext
+end
+
+#This is a scoping problem and something your browser knows how to handle.
+#You should not test your browser's ability to interpret placeholder and show it.
+Then(/^I should see Prompt text disappears when user begins typing in a topic$/) do
+  expect(find_field('text_input')['placeholder']).to eq ("Enter Topic")
+end
+
+Then(/^I should see Background color "([^"]*)"$/) do |color|
+  text_input = page.find_by_id("text_input")
+  expect(text_input.native.style('background-color')).to eq('rgba(255, 255, 255, 1)')
+end
+
+#Testing for Build Collage button
+When(/^build collage button loads$/) do
+  find_by_id("text_input")
+end
+
+Then(/^I should see Button labeled with text that says "([^"]*)"$/) do |text|
+  build_button = page.find_by_id("build")
+  expect(build_button.text).to eq(text)
+end
+
+Then(/^I should see Text color is "([^"]*)"$/) do |color|
+  build_button = page.find_by_id("build")
+  expect(build_button.native.style('color')).to eq('rgba(255, 255, 255, 1)')
+end
+
+Then(/^I should see Button color is "([^"]*)"$/) do |color|
+  build_button = page.find_by_id("build")
+  expect(build_button.native.style('background-color')).to eq('rgba(102, 102, 102, 1)')
 end
