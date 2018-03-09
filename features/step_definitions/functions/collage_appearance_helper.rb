c3f96330d02714c871cd12f92089d059b52c7066
Shing Tak Ryan To <shingtat@usc.edu>
2018-03-08 22:48:46 -0800

    final a5 a8

--- features/step_definitions/functions/collage_appearance_helper.rb
+++ features/step_definitions/functions/collage_appearance_helper.rb
@@ -46,3 +46,46 @@ end
 def fileSize(filename)
   return File.size(filename)
 end
+
+#FOR REQUIREMENT 5: Each photo has a 3 pixel white frame surrounding it
+def borderCheck(fileName)
+  content = IO.readlines(fileName)
+  rows = content.length
+  columns = content[0].length
+  for i in 0..rows-1
+    lineArr = content[i].split(" ")
+    if i < 3 || i >= rows-3
+      lineArr.each do |num|
+        return false if num!="0"
+      end
+
+    else
+      for i in 0..2
+        return false if lineArr[i]!="0"
+      end
+    end
+  end
+  return true
+end
+
+#FOR REQUIREMENT 5: Each photo has a 3 pixel white frame surrounding it
+def whiteBorderTest(fileDirectory)
+  counter = 0
+  for i in 1..30
+    return false if borderCheck(fileDirectory + "border" + i.to_s + ".txt")==false
+  end
+  return true
+end
+
+#FOR REQUIREMENT 8: No white (empty) space should be present in the collage
+def noWhiteSpace(fileName)
+  content = IO.readlines(fileName)
+  rows = content.length
+  for i in 0..rows-1
+    lineArr = content[i].split(" ")
+    lineArr.each do |num|
+      return false if num!="1"
+    end
+  end
+  return true
+end
